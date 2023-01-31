85e2c7e6d0e5e8ea8eaee85adf893b2be3e85dcb
Daxit godhani <dakshitgodhani103@gmail.com>
2023-01-31 21:46:10 +0530

    new category

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News'
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 export default class App extends Component {
-   c="daxit"
   render() {
     return (
       <div>
+        <Router>
         <Navbar/>
-        <News/>
+        <Routes>
+          <Route exact path='/' element={<News key="general" pagesize={12} category="general" counrty="in"/>} />
+          <Route exact path='/business' element={<News key="business" pagesize={12} counrty="in" category="business"/>} />
+          <Route exact path='/entertainment' element={<News key="entertainment" pagesize={12} category="entertainment" counrty="in"/>} />
+          <Route exact path='/health' element={<News key="health" pagesize={12} category="health" counrty="in"/>} />       
+          <Route exact path='/science' element={<News key="science" pagesize={12} category="science" counrty="in"/>} />
+          <Route exact path='/sports' element={<News key="sports" pagesize={12} category="sports" counrty="in"/>} />
+          <Route exact path='/technology' element={<News key="technology" pagesize={12} category="technology" counrty="in"/>} />
+        </Routes>
+       </Router>
       </div>
     )
   }
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,22 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
-    const {title,discription,imgurl,newsurl,date}=this.props
+    const { title, discription, imgurl, newsurl, source, date, author } =
+      this.props;
     return (
-      <div className='my-3'>
-        <div className="card" style={{width:"18rem"}}>
-          <img src={imgurl?imgurl:"https://c.ndtvimg.com/2018-10/dl56ce8_google-generic-reuters_625x300_08_October_18.jpg"} className="card-img-top" alt="..."/>
-            <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{discription}.</p>
-            <p className="card-text">{date}.</p>
-            <a href={newsurl} className="btn btn-sm btn-dark">Read MOre</a>
+      <div className="my-3">
+        <div className="card">
+          <img
+            src={
+              imgurl
+                ? imgurl
+                : "https://c.ndtvimg.com/2018-10/dl56ce8_google-generic-reuters_625x300_08_October_18.jpg"
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              {title}{" "}
+              <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                {source}
+                <span class="visually-hidden">unread messages</span>
+              </span>
+            </h5>
+            <p className="card-text">
+              {discription
+                ? discription
+                : "------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"}
+            </p>
+            <p className="card-text">
+              By {author ? author : "unknow author"} <br /> on{" "}
+              {new Date(date).toGMTString()}.
+            </p>
+            <a href={newsurl} className="btn btn-sm btn-dark">
+              Read MOre
+            </a>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Newsitem
+export default Newsitem;
